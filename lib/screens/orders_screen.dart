172003dcc9080e1a04c7f4776ccea7f981da4278
pritam-dante <pritam.dante@gmail.com>
2020-06-23 00:27:59 +0530

    futureBuilder Method

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -6,47 +6,69 @@ import '../providers/orders.dart' show Orders;
 import '../widgets/order_item.dart';
 import '../widgets/app_drawer.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static const routeName = '/orders';
+  //changing back to stateless widget because our futureBUILDER method makes it easy
 
-  @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
+//   @override
+//   _OrdersScreenState createState() => _OrdersScreenState();
+// }
 
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
+// class _OrdersScreenState extends State<OrdersScreen> {
+//   var _isLoading = false;
 
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
+  // @override
+  // void initState() {
+  //   //Future.delayed(Duration.zero).then((_) async {
+  //   //setState(() {
+  //   //in ProviderOfContext we can use listen false so that we dont have to use futureDelayed method
+  //   _isLoading = true;
+  //   // });
+  //   Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
+  //     setState(() {
+  //       _isLoading = false;
+  //     });
+  //   });
+
+  //   super.initState();
+  // }
+
+//With FUTURE_BUILDER method without using initState or didChangeDependencies
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
-
+    // final orderData = Provider.of<Orders>(context);
+    //cz we will get infinite loop
+    //better use consumer
     return Scaffold(
       appBar: AppBar(
         title: Text('Your orders'),
       ),
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemBuilder: (ctx, index) => OrderItem(orderData.orders[index]),
-              itemCount: orderData.orders.length,
-            ),
+      body: FutureBuilder(
+        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            if (dataSnapshot.error != null) {
+              //...
+              // error handeling stuff
+              return Center(
+                child: Text('An error occured'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemBuilder: (ctx, index) =>
+                      OrderItem(orderData.orders[index]),
+                  itemCount: orderData.orders.length,
+                ),
+              );
+            }
+          }
+        },
+      ),
     );
   }
 }
